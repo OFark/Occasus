60ddd7061c341760d052b207f97940fabbde26cf
AzureAD\TodPalin <Tod@ThePalins.co.uk>
2022-11-23 16:41:19 +0000

    Moved FeatureManagement extensions

--- Occasus.FeatureManagement/Extensions.cs
+++ Occasus.FeatureManagement/Extensions.cs
@@ -1,18 +1,33 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.FeatureManagement;
+using Occasus.Options;
+using Occasus.Repository.Interfaces;
 
 namespace Occasus.FeatureManagement
 {
     public static class Extensions
     {
-        public static OptionsBuilder<TOption> UseFeatureFlags<TOption>(this OptionsBuilder<TOption> optionBuilder) where TOption : class, new()
+        public static IOptionsStorageRepositoryWithServices WithFeatureFlagOptions<TOption>(this IOptionsStorageRepositoryWithServices storageRepository, IConfiguration configuration) where TOption : class, new()
+        => storageRepository.WithFeatureFlagOptions<TOption>(configuration, out _);
+
+        public static IOptionsStorageRepositoryWithServices WithFeatureFlagOptions<TOption>(this IOptionsStorageRepositoryWithServices storageRepository, IConfiguration configuration, out OptionsBuilder<TOption> optionBuilder) where TOption : class, new()
         {
-            optionBuilder.Configure<IConfiguration>((settings, config) =>
-            {
-                optionBuilder.Services.AddFeatureManagement(config);
-            });
-            return optionBuilder;
+            var services = storageRepository.Services;
+            services.AddFeatureManagement(configuration.GetSection(typeof(TOption).Name));
+
+            return storageRepository.WithOptions(out optionBuilder); ;
+        }
+
+        public static IServiceCollection WithFeatureFlagOptions<TOption>(this IServiceCollection services, IConfiguration configuration) where TOption : class, new()
+            => services.WithFeatureFlagOptions<TOption>(configuration, out _);
+
+        public static IServiceCollection WithFeatureFlagOptions<TOption>(this IServiceCollection services, IConfiguration configuration, out OptionsBuilder<TOption> optionBuilder) where TOption : class, new()
+        {
+            services.AddFeatureManagement(configuration.GetSection(typeof(TOption).Name));
+
+            return services.WithOptions(out optionBuilder);
         }
     }
 }
